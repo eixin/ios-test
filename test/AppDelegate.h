539dfea1ad6ecf35d0a0732ede34fa65ea73d4c2
eixin <nixie.ua@gmail.com>
2019-04-14 19:37:13 +0200

    initial commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Home on 01/04/2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Home on 01/04/2019.
+//  Copyright © 2019 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
